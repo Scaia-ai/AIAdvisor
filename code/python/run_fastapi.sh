0cad89b4b8aa452f9158efe3dd9a3b256a5145a0
Mark Kerzner <mark@elephantscale.com>
2024-01-06 22:15:45 -0600

    run fast api

--- code/python/run_fastapi.sh
+++ code/python/run_fastapi.sh
@@ -1,3 +1,3 @@
 # Start a code application for code
 # uvicorn  main:app --reload --host 0.0.0.0 &
-gunicorn main:app -w 3 -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker --threads 2
+gunicorn main:app -w 3 -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker --threads 1
