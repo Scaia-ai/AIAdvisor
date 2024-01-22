bca19260fd1b803465356dbfe79e7f2fb7ad3246
Mark Kerzner <mark@elephantscale.com>
2024-01-22 16:11:36 -0600

    v

--- code/python/global_config.py
+++ code/python/global_config.py
@@ -3,7 +3,7 @@ import logging
 from dotenv import load_dotenv
 load_dotenv()
 
-AI_ADVISOR_VERSION = "1.0.3"
+AI_ADVISOR_VERSION = "10.1.3"
 LLM_MODEL = "gpt-4"
 LOG_FILE_NAME = "aiadvisor.log"
 LOG_LEVEL = logging.DEBUG
