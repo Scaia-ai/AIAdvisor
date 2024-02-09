1b73124c7cea845e2d39bcf0486ed692ab84b890
Mark Kerzner <mark@elephantscale.com>
2024-02-09 08:07:30 -0600

    cleanup

--- code/python/run_fastapi.sh
+++ code/python/run_fastapi.sh
@@ -1,4 +1,2 @@
 # Start a code application for code
 uvicorn  main:app --reload --host 0.0.0.0 &
-# gunicorn main:app -w 3 -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker --threads 1
-# gunicorn main:app -w 3 -b 0.0.0.0:8000 --threads 1
