520207a182c9257b083c23a10de6bef682c2969c
Mark Kerzner <mark@elephantscale.com>
2024-02-13 14:44:25 -0600

    Model as env parameter

--- code/python/global_config.py
+++ code/python/global_config.py
@@ -3,8 +3,8 @@ import logging
 from dotenv import load_dotenv
 load_dotenv()
 
-AI_ADVISOR_VERSION = "10.2.3"
-LLM_MODEL = "gpt-4-1106-preview"
+AI_ADVISOR_VERSION = "10.2.4"
+LLM_MODEL = os.getenv("LLM_MODEL")
 LOG_FILE_NAME = "aiadvisor.log"
 LOG_LEVEL = logging.DEBUG
 PINECONE_INDEX = os.getenv("PINECONE_INDEX_NAME")
