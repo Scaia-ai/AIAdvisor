1e93e93677bcc1c6b0d382e417419e360ddb051d
Marlon <masj1a@gmail.com>
2024-03-21 10:07:07 -0500

    Fix

--- code/python/aiadvisor_pinecone_lch.py
+++ code/python/aiadvisor_pinecone_lch.py
@@ -25,37 +25,14 @@ logger = logging.getLogger(global_config.LOG_FILE_NAME)
 
 _ = load_dotenv(find_dotenv())  # read local .env file
 openai.api_key = os.getenv('OPENAI_API_KEY')
+MODEL = "gpt-4"
 index_name = os.getenv('PINECONE_INDEX_NAME')
 
 # initialize pinecone
 pc = Pinecone(api_key=os.getenv('PINECONE_API_KEY'))
 
-
-embeddings = OpenAIEmbeddings(openai_api_key=openai.api_key)
-index_lch = pc.from_existing_index(index_name, embeddings, namespace = "159")
-
-
-def get_similar_docs(query,namespace,num_sources=5,score=False):
-  if score:
-    similar_docs = index_lch.similarity_search_with_score(query, k=num_sources, namespace=namespace)
-  else:
-    similar_docs = index_lch.similarity_search(query,k=num_sources, namespace=namespace)
-  return similar_docs
-
-
-llm = OpenAI(model_name=MODEL)
-chain = load_qa_chain(llm, chain_type="stuff")
-
-
-def get_answer(query, namespace):
-    similar_docs_list = get_similar_docs(query, namespace=namespace)
-    # print(similar_docs)
-    return chain.run(input_documents=similar_docs_list, question=query)
-
-
 def clean_namespace(namespace: str):
     index_pc = pc.Index(os.getenv('PINECONE_INDEX_NAME'))
     index_pc.delete(deleteAll=True, namespace=namespace)
-
 
 
